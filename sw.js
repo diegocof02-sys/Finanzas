5cb12c7b2c8038750ef59799b74e67f8bf615dc3
Diego Cohen O'Farrill <diego@MacBook-Air-de-Diego.local>
2026-05-30 12:56:35 -0600

    fix PWA para GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'finanzas-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const CACHE = 'finanzas-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
